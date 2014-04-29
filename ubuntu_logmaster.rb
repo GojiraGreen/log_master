4633d73cfd644a6bfbb83ee172408624a7a0e339
Gojira <aronfate@gmail.com>
2014-04-29 23:28:20 +0200

    update

--- ubuntu_logmaster.rb
+++ ubuntu_logmaster.rb
@@ -6,71 +6,72 @@
 # For system logs only #
 
 class LogMaster
-	def initialize(file)
-		@file = file
-	end
+  def initialize(file)
+    @file = file
+  end
 
-	def frequency
-		tab = Array.new(24){|x| x=0}
+  def frequency
+    tab = Array.new(24){|x| x=0}
 
-		@file.each do |line|
-			line = line.split
-			(0..9).each do |i|
-				if(line[2] =~ /^0#{i}/)
-					tab[i] +=1
-				end
-			end
-			(10..23).each do |i|
-				if(line[2] =~ /^#{i}/)
-					tab[i] +=1
-				end
-			end
-		end
-	
-		for i in 0..23
-			if(tab[i] != 0)
-				puts "#{tab[i]} timestamps at #{i} o'clock.\n"
-			end
-		end
-	end
+    @file.each do |line|
+      line = line.split
+      (0..9).each do |i|
+        if(line[2] =~ /^0#{i}/)
+          tab[i] +=1
+        end
+      end
+
+      (10..23).each do |i|
+        if(line[2] =~ /^#{i}/)
+          tab[i] +=1
+        end
+      end
+    end
+
+    for i in 0..23
+      if(tab[i] != 0)
+        puts "#{tab[i]} timestamps at #{i} o'clock.\n"
+      end
+    end
+  end
 
 
-	def occurence(opt=nil)
-		count = {}
-		@file.each do |line|
-			line = line.split(/\d+:\d+:\d+/)[1]
-			line = line.split
-			line.shift if line[0] =~ /gojira/	#user name
-			unless opt.nil?
-				line[0].slice!(/\[.*\]/)
-			end
-			if count.has_key?(line[0])
-				count[line[0]] += 1
-			else
-				count[line[0]] = 1
-			end
-		end
+  def occurence(opt=nil)
+    count = {}
+    @file.each do |line|
+      line = line.split(/\d+:\d+:\d+/)[1]
+      line = line.split
+      line.shift if line[0] =~ /gojira/	#user name
+      unless opt.nil?
+        line[0].slice!(/\[.*\]/)
+      end
+      if count.has_key?(line[0])
+        count[line[0]] += 1
+      else
+        count[line[0]] = 1
+      end
+    end
 
-		count.each do |key, val|
-			puts "#{val} times #{key}"
-		end
-	end
+    count.each do |key, val|
+      puts "#{val} times #{key}"
+    end
+  end
 end
 
 
 file = File.open(ARGV.shift)
 script = LogMaster.new(file)
 
-print "\nOPTIONS:\noc -occurance of logs by hour\nocn -occurance with no PID\nfr -frequency of logs\n=> "
+print "\nOPTIONS:\nocu -occurance of logs by hour\nocn -occurance with no PID\nfrq -frequency of logs\n=> "
 STDOUT.flush
 opt = gets.chomp
 
-if opt == "oc"
-	script.occurence
+if opt == "ocu"
+  script.occurence
 elsif opt == "ocn"
-	script.occurence(true)
-elsif opt == "fr"
-	script.frequency
+  script.occurence(true)
+elsif opt == "frq"
+  script.frequency
 else
-	p "arg error"
+  p "arg error"
 end
